61f03838110d8c3a77e3218320d8d6528e55cba0
samuel Damilola <samfrexz69@gmail.com>
2023-02-16 06:22:39 -0800

    add 101-intel file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - the entry point
+ *
+ * Return: always 1 on success
+*/
+int main(void)
+{
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, text, 59);
+	return (1);
+}
