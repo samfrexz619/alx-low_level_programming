15dfd2df989fd69b499e995fa8968a68aab1b8d3
samuel Damilola <samfrexz69@gmail.com>
2023-02-17 06:20:43 -0800

    add file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	(n > 0)
 	? printf("%d is positive\n", n)
-	(n == 0)
+	: (n == 0)
 	? printf("%d is zero\n", n)
 	: printf("%d is negative\n", n);
 	return (0);
