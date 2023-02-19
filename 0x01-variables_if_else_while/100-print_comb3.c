2cb7f62ef0fda088e93c9a5f42c224953bb29fae
samuel Damilola <samfrexz69@gmail.com>
2023-02-18 23:03:16 -0800

    update file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,22 @@
 */
 int main(void)
 {
-	int num[10] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
-
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 48; num1 <= 56; num1++)
 	{
-		for (num2 = 1; num2 < 10; num2++)
+		for (num2 = 49; num2 <= 57; num2++)
 		{
-			if (num1 != num2 && num1 < num2)
+			if (num1 < num2)
 			{
-				putchar(num[num1]);
-				putchar(num[num2]);
+				putchar(num1);
+				putchar(num2);
 
-				if (num1 == num2)
+				if (num1 != 56 || num2 != 57)
 				{
-					continue;
+					putchar(',');
+					putchar(' ');
 				}
-				putchar(',');
-				putchar(' ');
 			}
 		}
 	}
