cd7aab779aa7582113af31f337a1f2b4356e10f5
samuel Damilola <samfrexz69@gmail.com>
2023-02-21 10:51:56 -0800

    add file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ *
+ * @n: The arg passed
+ *
+ * Return: print the last digit.
+ */
+int print_last_digit(int n)
+{
+	x = n % 10;
+
+	if (x < 0)
+	{
+		_putchar(-x + 48);
+		return (-x);
+	}
+	else
+	{
+		_putchar(x + 48);
+		return (x);
+	}
+}
