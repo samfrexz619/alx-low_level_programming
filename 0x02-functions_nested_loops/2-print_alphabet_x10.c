5434b9270aff0048c24d2d10cfc7f341b53c577c
samuel Damilola <samfrexz69@gmail.com>
2023-02-21 09:01:21 -0800

    upd file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet in lowercase.
+ * print_alphabet_x10 - print alphabet in lowercase.
  *
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
 
