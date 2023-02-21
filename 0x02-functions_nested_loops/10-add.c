d1444b316272d4f11e7a25912a6ef2fb963f2296
samuel Damilola <samfrexz69@gmail.com>
2023-02-21 11:33:57 -0800

    add 10-add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ *
+ * @x: value 1
+ * @y: value 2
+ * Return: add two integers
+*/
+int add(int x, int y)
+{
+	int res = x + y;
+
+	return (res);
+}
