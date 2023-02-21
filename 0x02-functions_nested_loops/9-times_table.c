f8176c1f1dc34aefdfca4d3123157e24a4f6c57d
samuel Damilola <samfrexz69@gmail.com>
2023-02-21 11:24:50 -0800

    add file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - print times table from 0
+ *
+ * Return: no return value
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(48);
+		for (y = 1; y <= 9; y++)
+		{
+			z = x * y;
+			_putchar(44);
+			_putchar(32);
+			if (z <= 9)
+			{
+				_putchar(32);
+				_putchar(z + 48);
+			}
+			else
+			{
+				_putchar((z / 10) + 48);
+				_putchar((z % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
