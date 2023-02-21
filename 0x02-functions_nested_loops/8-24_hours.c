1d32bbb9209e0553e2f9cb65f0cbedf0289816ed
samuel Damilola <samfrexz69@gmail.com>
2023-02-21 11:07:19 -0800

    add file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints the minutes of a day
+ *
+ * Return: no return value
+ */
+void jack_bauer(void)
+{
+	int w, x, y, z;
+
+	for (w = 48; w <= 50; w++)
+	{
+		for (x = 48; x <= 57; x++)
+		{
+			for (y = 48; y <= 53; y++)
+			{
+				for (z = 48; z <= 57; z++)
+				{
+					if (w >= 50 && x >= 52)
+						break;
+					_putchar(w);
+					_putchar(x);
+					_putchar(58);
+					_putchar(y);
+					_putchar(z);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
