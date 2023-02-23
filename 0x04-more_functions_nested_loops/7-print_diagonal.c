87e1f3fd69460027d351c30b03d74aaa8fac9014
samuel Damilola <samfrexz69@gmail.com>
2023-02-23 09:15:08 -0800

    add file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "mainn.h"
+
+/**
+ * print_diagonal - prints diagonal line n times.
+ * @n: times diagonal.
+ * Return: no return.
+ */
+void print_diagonal(int n)
+{
+	int index, j;
+
+	for (index = 0; index < n; index++)
+	{
+		for (j = 0; j < index; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (index < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
