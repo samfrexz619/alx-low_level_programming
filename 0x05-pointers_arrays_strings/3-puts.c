4cd6291cb8163371d826f19a4ed4eeed4a46ea64
samuel Damilola <samfrexz69@gmail.com>
2023-02-27 12:09:38 -0800

    upd file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,17 +7,8 @@
  */
 void _puts(char *str)
 {
-	int counter = 0;
+	char arr[] = str;
 
-	while (counter >= 0)
-	{
-		if (str[counter] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[counter]);
-		count++;
-	}
+	puts(arr);
 }
 
