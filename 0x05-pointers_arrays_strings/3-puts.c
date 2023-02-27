d775199d05e06a590fe0165778ecd88ce4a3c626
samuel Damilola <samfrexz69@gmail.com>
2023-02-27 12:25:18 -0800

    upd file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <string.h>
+#include <stdlib.h>
 /**
  * _puts - prints a string, followed by a new line.
  * @str: input string.
@@ -7,7 +8,9 @@
  */
 void _puts(char *str)
 {
-	char arr[] = str;
+	char arr[strlen(str) + 1];
+
+	strcpy(arr, str);
 
 	puts(arr);
 }
