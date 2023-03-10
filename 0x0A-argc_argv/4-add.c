0782bd173bb335dc6f44741081b8af29686529ba
samuel Damilola <samfrexz69@gmail.com>
2023-03-10 11:30:28 -0800

    add file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds positive numbers.
+ * @argc: number of cmd line args.
+ * @argv: array that contains the program cmd line args.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int index, b, add = 0;
+
+	for (index = 1; index < argc; index++)
+	{
+		for (b = 0; argv[index][j] != '\0'; b++)
+		{
+			if (!isdigit(argv[index][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[index]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
