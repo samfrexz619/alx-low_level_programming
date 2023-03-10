3c015a8a71e0cacc5a08edbc4185551e59fa1e7e
samuel Damilola <samfrexz69@gmail.com>
2023-03-10 09:43:59 -0800

    add files

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence.
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
+ * NULL if the substring is not found.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *ahaystack;
+	char *bneedle;
+
+	while (*haystack != '\0')
+	{
+		ahaystack = haystack;
+		bneedle = needle;
+
+		while (*haystack != '\0' && *bneedle != '\0' && *haystack == *bneedle)
+		{
+			haystack++;
+			bneedle++;
+		}
+		if (!*bneedle)
+			return (ahaystack);
+		haystack = ahaystack + 1;
+	}
+	return (0);
+}
+
