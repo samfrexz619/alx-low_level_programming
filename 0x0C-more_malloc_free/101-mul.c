2515df97ce8105ec6bdc44aa4e203f2e17648ac5
samuel Damilola <samfrexz69@gmail.com>
2023-03-17 08:01:11 -0700

    add file

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ *
+ * Return: no return.
+ */
