f7280ee1f9c80c11f74cf7726b37548b86c2b582
samuel Damilola <samfrexz69@gmail.com>
2023-04-12 03:59:02 -0700

    upd file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,8 +10,8 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_addr(char *ptr)
-void print_type(char *ptr)
-void print_osabi(char *ptr)
+void print_addr(char *ptr);
+void print_type(char *ptr);
+void print_osabi(char *ptr);
 
 #endif
