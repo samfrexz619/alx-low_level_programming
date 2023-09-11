dfa0313ff1d61eae34d09c81a0402f6a7e117f18
samuel Damilola <samfrexz69@gmail.com>
2023-09-11 06:02:09 -0700

    add file

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - searches for a value
+ * @array: input array
+ * @size: array size
+ * @value: value to search
+ * Return: index
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int index, j, k, prev;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	j = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * j;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+	}
+
+	return (-1);
+}
